dc416f51b313380d85b103c75562c61421e27e45
Ankit Parmar <ankit.p@addwebsolution.in>
2025-10-10 19:55:20 +0530

    build prefix removed

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -24,7 +24,7 @@ function recursiveLayouts(route) {
 }
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   scrollBehavior(to) {
     if (to.hash) return { el: to.hash, behavior: 'smooth', top: 60 };
     return { top: 0 };
