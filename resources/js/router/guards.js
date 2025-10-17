3b21017fffae342ea473094ef0308204533ff1db
Ankit Parmar <ankit.p@addwebsolution.in>
2025-10-17 12:32:42 +0530

    guard no auth fix

--- resources/js/router/guards.js
+++ resources/js/router/guards.js
@@ -4,7 +4,7 @@ import { canNavigate } from '@layouts/plugins/casl'
 export const setupGuards = router => {
 router.beforeEach((to) => {
   const token = useCookie('accessToken').value
-  const roleId = (useCookie('role_id').value)
+  const roleId = Number(useCookie('role_id').value)
   const isLoggedIn = !!token
 
   if (to.meta.public || to.name === 'login') return true
